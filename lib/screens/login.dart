266cc79168a1754b256946c0872fd4af22782e48
Cesar Acosta <c3sar.adan@gmail.com>
2024-05-20 10:29:40 -0700

    20/05/2024

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,163 +1,134 @@
 import 'package:flutter/material.dart';
 import 'package:login_frontend_20241/Services/auth_services.dart';
 import 'package:login_frontend_20241/Services/notifications_services.dart';
-import 'package:login_frontend_20241/providers/login_form_provider.dart';
+import 'package:login_frontend_20241/ui/input_decoration.dart';
+import 'package:login_frontend_20241/widgets/card_container.dart';
 import 'package:provider/provider.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+import '../providers/login_form_provider.dart';
 
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: ChangeNotifierProvider(
-          create: (_) => LoginF_Pfrovider(),
-          child: _LoginForm(),
-        ),
+        //body: AuthBackground(
+        body: Container(
+            child: SingleChildScrollView(
+      child: Column(
+        children: [
+          const SizedBox(height: 250),
+          CardContainer(
+              child: Column(
+            children: [
+              const SizedBox(height: 10),
+              Text('Login', style: Theme.of(context).textTheme.headline4),
+              const SizedBox(height: 30),
+              ChangeNotifierProvider(
+                  create: (_) => LoginF_Pfrovider(), child: _LoginForm())
+            ],
+          )),
+          const SizedBox(height: 50),
+          TextButton(
+              onPressed: () =>
+                  Navigator.pushReplacementNamed(context, 'register'),
+              style: ButtonStyle(
+                  overlayColor: MaterialStateProperty.all(
+                      Colors.redAccent.withOpacity(0.1)),
+                  shape: MaterialStateProperty.all(StadiumBorder())),
+              child: const Text(
+                'Crear una nueva cuenta',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              )),
+          const SizedBox(height: 50),
+        ],
       ),
-    );
+    )));
   }
 }
 
 class _LoginForm extends StatelessWidget {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  _LoginForm({super.key});
-
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     final loginForm = Provider.of<LoginF_Pfrovider>(context);
-    return Center(
-      child: Scaffold(
-        body: DecoratedBox(
-          decoration: const BoxDecoration(
-            color: Color.fromARGB(255, 96, 108, 93),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const SizedBox(
-                  height: 16,
-                ),
-                Container(
-                  width: size.width * 0.80,
-                  height: size.height * 0.17,
-                  alignment: Alignment.center,
-                  decoration: const BoxDecoration(
-                      //TODO: Poner imagen
-                      //image: DecorationImage(image: Icon(icon))
-                      ),
-                ),
-                Container(
-                  width: size.width * 0.80,
-                  height: size.height * 0.05,
-                  alignment: Alignment.center,
-                ),
-                TextFormField(
-                  autocorrect: false,
-                  keyboardType: TextInputType.emailAddress,
-                  controller: _emailController,
-                  decoration: const InputDecoration(
-                      hintText: 'ejemplo@test.com',
-                      labelText: 'Email',
-                      labelStyle: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                          color: Color.fromARGB(255, 255, 244, 244))),
-                  validator: (value) {
-                    String pattern =
-                        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-                    RegExp regExp = RegExp(pattern);
-                    return regExp.hasMatch(value ?? '')
-                        ? null
-                        : 'El valor ingresado no es un correo';
-                  },
-                ),
-                TextFormField(
-                  autocorrect: false,
-                  controller: _passwordController,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                      hintText: '**************',
-                      labelText: 'Contraseña',
-                      labelStyle: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                          color: Color.fromARGB(255, 255, 244, 244))),
-                  validator: (value) {
-                    return (value != null && value.length >= 8)
-                        ? null
-                        : 'La contraseña debe ser mayor a 7 caracteres';
-                  },
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextButton(
-                  onPressed: () {
-                    print('Olvidaste contraseña');
-                  },
-                  child: const Text(
-                    '¿Olvidaste tu contraseña?',
-                    style: TextStyle(color: Color.fromARGB(255, 255, 244, 244)),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: loginForm.isLoading
-                      ? null
-                      : () async {
-                          final authService =
-                              Provider.of<AuthService>(context, listen: false);
-                          if (!loginForm.isValidForm()) return;
-                          final String? errorMessage = await authService.login(
-                              loginForm.email, loginForm.password);
-                          if (errorMessage == null) {
-                            Navigator.pushReplacementNamed(context, 'home');
-                          } else {
-                            NotificationsServices.showSnackbar(errorMessage);
-                            loginForm.isLoading = false;
-                          }
-                        },
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          Color.fromARGB(255, 255, 244, 244))),
-                  child: const Text(
-                    'Iniciar sesión',
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.w500,
-                      color: Color.fromARGB(255, 96, 100, 93),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                      Color.fromARGB(255, 255, 244, 244),
-                    ),
-                  ),
-                  onPressed: () {
-                    Navigator.pushNamed(context, 'register', arguments: '');
-                  },
-                  child: const Text(
-                    'Regístrate',
-                    style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.w500,
-                        color: Color.fromARGB(255, 96, 108, 93)),
-                  ),
-                ),
-              ],
+
+    return Container(
+      child: Form(
+        key: loginForm.formKey,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                  hintText: 'john.doe@gmail.com',
+                  labelText: 'Correo electrónico',
+                  prefixIcon: Icons.alternate_email_rounded),
+              onChanged: (value) => loginForm.email = value,
+              validator: (value) {
+                String pattern =
+                    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                RegExp regExp = new RegExp(pattern);
+
+                return regExp.hasMatch(value ?? '')
+                    ? null
+                    : 'El valor ingresado no luce como un correo';
+              },
             ),
-          ),
+            const SizedBox(height: 30),
+            TextFormField(
+              autocorrect: false,
+              obscureText: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                  hintText: '*****',
+                  labelText: 'Contraseña',
+                  prefixIcon: Icons.lock_outline),
+              onChanged: (value) => loginForm.password = value,
+              validator: (value) {
+                return (value != null && value.length >= 6)
+                    ? null
+                    : 'La contraseña debe de ser de 6 caracteres';
+              },
+            ),
+            const SizedBox(height: 30),
+            MaterialButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                disabledColor: Colors.grey,
+                elevation: 0,
+                color: Colors.redAccent,
+                child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                    child: Text(
+                      loginForm.isLoading ? 'Espere' : 'Ingresar',
+                      style: TextStyle(color: Colors.white),
+                    )),
+                onPressed: loginForm.isLoading
+                    ? null
+                    : () async {
+                        FocusScope.of(context).unfocus();
+                        final authService =
+                            Provider.of<AuthService>(context, listen: false);
+
+                        if (!loginForm.isValidForm()) return;
+
+                        loginForm.isLoading = true;
+
+                        // TODO: validar si el login es correcto
+                        final String? errorMessage = await authService.login(
+                            loginForm.email, loginForm.password);
+
+                        if (errorMessage == null) {
+                          Navigator.pushReplacementNamed(context, 'home');
+                        } else {
+                          // TODO: mostrar error en pantalla
+                          // print( errorMessage );
+                          NotificationsServices.showSnackbar(errorMessage);
+                          loginForm.isLoading = false;
+                        }
+                      })
+          ],
         ),
       ),
     );
--- lib/screens/principal.dart
+++ lib/screens/principal.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:login_frontend_20241/Services/auth_services.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(const PrincipalScr());
 
@@ -7,12 +9,19 @@ class PrincipalScr extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final authService = Provider.of<AuthService>(context, listen: false);
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
+            title: const Text('Material App Bar'),
+            leading: IconButton(
+              icon: const Icon(Icons.login_outlined),
+              onPressed: () {
+                authService.logout();
+                Navigator.pushReplacementNamed(context, 'login');
+              },
+            )),
         body: const Center(
           child: Text('Hello World'),
         ),
